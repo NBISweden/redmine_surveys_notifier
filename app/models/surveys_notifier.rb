77bba257725387c2b6de424fd4b96b70d6c4b752
Airen Zaldivar <airenzp@gmail.com>
2020-06-24 16:36:47 +0200

    solving bug detected when no survey field is defined

--- app/models/surveys_notifier.rb
+++ app/models/surveys_notifier.rb
@@ -27,20 +27,21 @@ private
 
   def self.manage_survey(issue)
     ::Rails.logger.info("Sending survey... " )
-
+    send = false
+    pi_name = ""
+    pi_email = ""
     issue.custom_field_values.each do |field|
       ::Rails.logger.info("custom field " + field.custom_field.name + " " + field.value)
       if field.custom_field.name == 'Principal Investigator' then
-        @pi_name = field.value
+        pi_name = field.value
       elsif field.custom_field.name == 'PI e-mail' then
-        @pi_email = field.value
+        pi_email = field.value
       elsif field.custom_field.name == 'Send survey when closed' then
-        @send = field
+        send = field.true?
       end
     end
-    if @send.true? then
-      SurveyMailer.survey_email(@pi_name, @pi_email, self.survey_html).deliver
-      
+    if send then
+      SurveyMailer.survey_email(pi_name, pi_email, self.survey_html).deliver
 
     end
   end
